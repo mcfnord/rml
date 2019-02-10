869b02dedd047c5f37702db8fd593c3fe54bb911
jrd <mcfnord@gmail.com>
2019-02-10 01:47:33 -0800

    not tested

--- numToWords.cs
+++ numToWords.cs
@@ -32,7 +32,7 @@ static void Main(string[] args)
         // we construct the tens by adding ty to something. Except eighty!
         string tensy = writtenDigit[tenth] ;
         Console.Write(tensy) ;
-        if(tensy[tensy.Length - 1 ] == 't')
+        if(tenth == 8)
             Console.Write('y') ;
         else
             Console.Write("ty") ;
